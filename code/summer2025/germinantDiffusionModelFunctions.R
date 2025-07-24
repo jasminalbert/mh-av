c4dfe94e71928ab8b94e98a31523d4b2e4c62239
jasminalbert <pimsupaa@uoregon.edu>
2025-07-24 11:40:57 -0700

    developing new model based on discussion w lauren yesterday

---
+++ code/summer2025/germinantDiffusionModelFunctions.R
@@ -0,0 +1,10 @@
+
+#From meeting on 7 23 25 w LH:
+#  Talked about using diffusion on a germinant model and movement of germinants is determined by discrete diffusion
+#Using a model with reduced parameters by scaling of lambda and alphas
+#Dont accumulate litter just make litter a fraction of previous years counts
+
+#scaled parameters
+
+
+
