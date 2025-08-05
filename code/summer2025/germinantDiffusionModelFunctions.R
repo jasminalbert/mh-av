22d07a712ce9deab2ea2364aee2f047bd763df7f
jasminalbert <pimsupaa@uoregon.edu>
2025-08-05 16:50:51 -0700

    made a nice figure

--- code/summer2025/germinantDiffusionModelFunctions.R
+++ code/summer2025/germinantDiffusionModelFunctions.R
@@ -150,7 +150,9 @@ for(s in dimnames(rd_ar)[[3]]){
     rd_ar[,,s,t] <- rnorm(gridsize^2)
   }
 }
-
+l1 <- 0.5;l2<-0.1
+rho <- 0.5
+theta <- 1
 #make a loop
 timesteps=10
 gridsize <- 2
@@ -189,11 +191,11 @@ for (t in 1:timesteps){
   randis2 <- randis2/sum(abs(randis2))#sum(randis2)
   sp1[,,"dispered",t] <- sp1[,,"seeds",t]-D*randis1
   sp10[,,"dispered",t] <- sp10[,,"seeds",t]-D*randis1
-  sp2[,,"dispered",t] <- sp2[,,"seeds",t]-D*litdifnorm
+  sp2[,,"dispered",t] <- sp2[,,"seeds",t]-D*theta*litdifnorm
   sp20[,,"dispered",t] <- sp20[,,"seeds",t]-D*randis2
   #deposit litter
-  lit1 <- sp1[,,"inds",t]*0.5
-  lit2 <- sp2[,,"inds",t]*0.1
+  lit1 <- sp1[,,"inds",t]*l1
+  lit2 <- sp2[,,"inds",t]*l2
   lit[,,t+1] <- lit1+lit2
   #t+1 inds are what dispersed
   sp1[,,"inds",t+1] <- sp1[,,"dispered",t]
@@ -203,7 +205,45 @@ for (t in 1:timesteps){
     sp20[,,"inds",t+1] <- sp20[,,"dispered",t]
   }
 }
+ncol <- 6;alpha_col <- 0.9
+cols1 <- hcl.colors(ncol,"PuBu",alpha_col,rev=F)
+cols2 <- hcl.colors(ncol,"OrRd",alpha_col,rev=F)
+col2rgb("darkgreen")
+gr <- rgb(0,100/255,0,0.15)
+i=1;j=1
+theta=1
+t=5
+pdf("../../figures/germinantDiffusion.pdf",width=9,height=7.5)
+par(mgp=c(2,0.1,0),mfrow=c(2,2),xpd=F,cex=.8,tcl=-.15,oma=c(1,.5,.5,0), mar=c(.8,1.3,.5,0))
+for (i in 1:2){
+  for (j in 1:2){
+    plot(1:t,seq(0,1,length.out=t),type="n",ylab='',xlab='',xlim=c(1,t),
+         ylim=c(0,0.8))
+    polygon(x=c(1:t-.3,t*1.02,t*1.02,t:1-.3),y=c(rep(-0.05,6),rev(lit[i,j,])[1],rev(lit[i,j,])),
+            col=gr,border=NA)
+    lines(1:t-.3,lit[i,j,],col="darkgreen",type='b',pch=3,cex=0.6,lwd=0.4)
+    lines(1:t+.5,sp1[i,j,"dispered",],type='b',col=cols1[3],lwd=0.5,pch=17)
+    lines(1:t+.5,sp2[i,j,"dispered",],type='b',col=cols2[3],lwd=0.5,pch=17)
+    lines(1:t+.5,sp10[i,j,"dispered",],type='b',col=cols1[3],lwd=0.5,pch=2,lty=3,cex=0.7)
+    lines(2:t+.5,sp20[i,j,"dispered",2:t],type='b',col=cols2[3],lwd=0.5,pch=2,lty=3,cex=0.7)
+    lines(sp1[i,j,"inds",],type='b',col=cols1[1])
+    lines(2:t,sp2[i,j,"inds",2:t],type='b',col=cols2[1])
+    lines(sp10[i,j,"inds",],type='b',lty=2,col=cols1[1])
+    lines(2:t,sp20[i,j,"inds",2:t],type='b',col=cols2[1],lty=2)
+    points(1:t+.2,sp1[i,j,"seeds",],pch=18,col=cols1[2])
+    points(1:t+.2,sp2[i,j,"seeds",],pch=18,col=cols2[2])
+    points(1:t+.2,sp10[i,j,"seeds",],pch=5,col=cols1[2],cex=0.7)
+    points(1:t+.2,sp20[i,j,"seeds",],pch=5,col=cols2[2],cex=0.7)
+    
+    #legend("topleft", legend=c("sp1","sp1notlit","seedslit","seedsnolit",
+#                               "disp_lit","dispNolit","lit"),col=c(1,"darkred","blue","orange",
+ #                                                                  "skyblue","tan",3),lty=1,bty="n")
+  }
+}
+title(main=paste(parms,pnames,sep="=",collapse=" "),outer=T,font.main=1,line=-.3)
+dev.off()
 
-
+parms <- c("lambda1","lambda2","alpha","beta","rho","theta","l1","l2","D")
+pnames <- c(lambda1,lambda2,alpha,beta,rho,theta,l1,l2,D)
 
 
