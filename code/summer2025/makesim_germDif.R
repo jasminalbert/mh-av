6943e4cea3efb20bf0949d0239f56ab22351cfab
jasminalbert <pimsupaa@uoregon.edu>
2025-08-14 11:23:21 -0700

    edits that allow for toggling burnin time and LamLit function

--- code/summer2025/makesim_germDif.R
+++ code/summer2025/makesim_germDif.R
@@ -1,33 +1,50 @@
 #working on (1) burn in monoculture
+#L:carry capacity (max lambda)
+#k: logistic growth rate, steepness of curve (rho)
+#x0: x value at functions midpoint (lambdai^0)
+#standard logistic function has L=k=1,x0=0
+logis <-function(x,L=1,k=1,x0=0){
+  denom <- 1+exp(-k * (x-x0))
+  y <- L/denom
+  return(y)
+}
 #storage array
 dimnames <- list(i=1:gridsize, j=1:gridsize,stage=c("inds","seeds","dispered"), 
                  time=1:(timesteps+1))
 sp1 <- array(NA, dim=c(gridsize,gridsize,3,timesteps+1), dimnames)
 sp2 <- sp1
-sp1[,,"inds",1] <- N1
-sp2[,,"inds",1] <- 0
-sp2[,,"inds",2] <- N2
 #litter
 lit <- sp1[,,1,]
+#initializing
+sp1[,,"inds",1] <- N10
+sp2[,,"inds",1] <- N20
+#sp2[,,"inds",2] <- N2
 lit[,,1] <- ifelse(is.na(N1),NA,0)
 
 #no litter models
 sp10 <- sp1; sp20 <- sp2
 t=1
 for (t in 1:timesteps){
-  #seed=sample(1:1e6,1)
+  if (t<tD){D<-D0} else{D<-D1}
+  if (t==tD){
+    sp1[,,"inds",t] <- N1+sp1[,,"inds",t]
+    sp10[,,"inds",t] <- N1+sp10[,,"inds",t]
+    sp2[,,"inds",t] <- N2+sp2[,,"inds",t]
+    sp20[,,"inds",t] <- N2+sp20[,,"inds",t]
+  }
+  #no lit at t=1
   litdif <- litDif(lit[,,t])
-  #litdifnorm <- litdif/sum(abs(litdif)) 
-  lambda1lit <- lambda1*exp(-litdif*rho)#no lit at t=1
+  if (LLfun =="logis"){lambda1lit <- logis(-litdif,L=(lambda1-1)*2,k=rho)+1}
+  if (LLfun == "exp"){lambda1lit <- lambda1*exp(-litdif*rho)}
   sp1[,,"seeds",t] <- lambda1lit*sp1[,,"inds",t]*(1-sp1[,,"inds",t]-alpha*sp2[,,"inds",t])
   sp10[,,"seeds",t] <- lambda1*sp10[,,"inds",t]*(1-sp10[,,"inds",t]-alpha*sp20[,,"inds",t])
   sp2[,,"seeds",t] <- lambda2*sp2[,,"inds",t]*(1-sp2[,,"inds",t]-beta*sp1[,,"inds",t])
   sp20[,,"seeds",t] <- lambda2*sp20[,,"inds",t]*(1-sp20[,,"inds",t]-beta*sp10[,,"inds",t])
-  zero <- mapply(function(x){x[,,"seeds",t]},list(sp1,sp10,sp2,sp20))<0
-  if (any(zero)){cat("\nZERO!",t)}
+  #zero <- mapply(function(x){x[,,"seeds",t]},list(sp1,sp10,sp2,sp20))<0
+  #if (any(zero)){cat("\nZERO!",t)}
   #mapply(function(x){x[,,"seeds",t]},list(sp1,sp10,sp2,sp20))
-  if(any(sp1[,,"seeds",t] <0)){sp1[,,"seeds",t][sp1[,,"seeds",t]<0]<-0}
-  if(any(sp2[,,"seeds",t] <0)){sp2[,,"seeds",t][sp2[,,"seeds",t]<0]<-0}
+  if(any(na.omit(sp1[,,"seeds",t]) <0)){sp1[,,"seeds",t][na.omit(sp1[,,"seeds",t])<0]<-0}
+  if(any(na.omit(sp2[,,"seeds",t]) <0)){sp2[,,"seeds",t][na.omit(sp2[,,"seeds",t])<0]<-0}
   #dispersal
 
   litdif[,] <- rank(-litdif)
@@ -43,10 +60,10 @@ for (t in 1:timesteps){
   #t+1 inds are what dispersed
   sp1[,,"inds",t+1] <- sp1[,,"dispered",t]
   sp10[,,"inds",t+1] <- sp10[,,"dispered",t]
-  if (t>1){
-    sp2[,,"inds",t+1] <- sp2[,,"dispered",t]
-    sp20[,,"inds",t+1] <- sp20[,,"dispered",t]
-  }
+  #if (t>1){
+  sp2[,,"inds",t+1] <- sp2[,,"dispered",t]
+  sp20[,,"inds",t+1] <- sp20[,,"dispered",t]
+  #}
 }
 litmaxes <- apply(lit[,,-1], 3, function(x) which(x==max(x),T ))
 litmax <- lit
@@ -67,17 +84,17 @@ for (i in 1:2){
     lines(1:t-.3,lit[i,j,1:t],col=ifelse(litmax[i,j,1:t],"limegreen","darkgreen"),type='b',pch=ifelse(litmax[i,j,1:t],8,3),
           cex=ifelse(litmax[i,j,1:t],1.5,0.6),lwd=0.4)
     lines(1:t+.5,sp1[i,j,"dispered",1:t],type='b',col=cols1[3],lwd=0.5,pch=17)
-    lines(2:t+.5,sp2[i,j,"dispered",2:t],type='b',col=cols2[3],lwd=0.5,pch=17)
+    lines(1:t+.5,sp2[i,j,"dispered",1:t],type='b',col=cols2[3],lwd=0.5,pch=17)
     lines(1:t+.5,sp10[i,j,"dispered",1:t],type='b',col=cols1[3],lwd=0.5,pch=2,lty=3,cex=0.7)
-    lines(2:t+.5,sp20[i,j,"dispered",2:t],type='b',col=cols2[3],lwd=0.5,pch=2,lty=3,cex=0.7)
+    lines(1:t+.5,sp20[i,j,"dispered",1:t],type='b',col=cols2[3],lwd=0.5,pch=2,lty=3,cex=0.7)
     lines(sp1[i,j,"inds",1:t],type='b',col=cols1[1])
-    lines(2:t,sp2[i,j,"inds",2:t],type='b',col=cols2[1])
+    lines(1:t,sp2[i,j,"inds",1:t],type='b',col=cols2[1])
     lines(sp10[i,j,"inds",1:t],type='b',lty=2,col=cols1[1])
-    lines(2:t,sp20[i,j,"inds",2:t],type='b',col=cols2[1],lty=2)
+    lines(1:t,sp20[i,j,"inds",1:t],type='b',col=cols2[1],lty=2)
     points(1:t+.2,sp1[i,j,"seeds",1:t],pch=18,col=cols1[2])
-    points(2:t+.2,sp2[i,j,"seeds",2:t],pch=18,col=cols2[2])
+    points(1:t+.2,sp2[i,j,"seeds",1:t],pch=18,col=cols2[2])
     points(1:t+.2,sp10[i,j,"seeds",1:t],pch=5,col=cols1[2],cex=0.7)
-    points(2:t+.2,sp20[i,j,"seeds",2:t],pch=5,col=cols2[2],cex=0.7)
+    points(1:t+.2,sp20[i,j,"seeds",1:t],pch=5,col=cols2[2],cex=0.7)
     
     #legend("topleft", legend=c("sp1","sp1notlit","seedslit","seedsnolit",
     #                               "disp_lit","dispNolit","lit"),col=c(1,"darkred","blue","orange",
--- code/summer2025/run_germDif.R
+++ code/summer2025/run_germDif.R
@@ -1,16 +1,27 @@
-timesteps=10
+timesteps=20
 gridsize <- 2
 parms <- c("lambda1","lambda2","alpha","beta","rho","theta","l1","l2","D")
 l1 <- 0.9;l2<-0.1
-rho <- 0.9;theta <- 1
+rho <- 1;theta <- 1
 D0 <- 0; D1 <- 0.5
+lammax <- 5
 lambda1 <- 2
 lambda2 <- 2
 alpha <- 0.5
 beta <- 0.5
-#make a loop
-#individuals at time 1
-N1 <- matrix(c(0.8,0.05,0.1,0.7),ncol=gridsize) 
-N2 <- 0.85-N1
+LLfun <- "exp" 
+#LLfun <- "logis"
 
+#individuals at time 1
+#N1 <- matrix(c(0.8,0.05,0.1,0.7),ncol=gridsize) 
+N10 <- matrix(c(0.8,0,0,0.7),ncol=gridsize) 
+N20 <- N10
+N20[N10==0] <- rev(N10[N10>0]);N20[N10>0] <-0
+#individuals introduced at time tD
+#tD <- 5
+tD <- 1
+N1 <- N20/4; N2 <- N10/4
+#pdf("../../figures/burnin_logislamlitsp0.9.pdf",width=9)
 source("./makesim_germDif.R")
+#dev.off()
+
