900158337fe7f7606f3ac54e9cacc6f83d3bb1a1
jasminalbert <pimsupaa@uoregon.edu>
2025-08-20 15:44:21 -0700

    color bar to indicate protion of outflow

--- code/summer2025/plotmovementfunction.R
+++ code/summer2025/plotmovementfunction.R
@@ -15,10 +15,12 @@ plotmovement <- function(){
 			if(dd[t-1]==0){x <-x-0.7}
 			out <- trackOr(dif)
 			arrows(x0=x+.1,y0=out$y0, y1=out$y1-.03*out$yd/abs(out$yd), x1=x+.9, length=0.1, lwd=out$weight,col="grey")
+			Pout <- ceiling(out$Pout*5)
+			pcols <- colPout(Pout,ifelse(t==timesteps,T,F))
 			if (percent==F){
-				mapply(text, x=x[1:nrow(out)]+exp(1/abs(out$yd)*.08)/4.25,y=out$yhlf+(out$y0-out$y1)/3, labels=round(out$out,6), srt=out$srt2,col="red")}#out$srt)#,col='white')
+				mapply(text, x=x[1]+exp(1/abs(out$yd)*.08)/4.25,y=out$yhlf+(out$y0-out$y1)/3, labels=round(out$out,5), cex=0.7, srt=out$srt2,col=pcols[Pout])}#out$srt)#,col='white'"red")
 			if (percent==T){
-				mapply(text, x=x[1]+exp(1/abs(out$yd)*.08)/4.25,y=out$yhlf+(out$y0-out$y1)/3, labels=paste(round(out$Pout*100,5),"%"), srt=out$srt2,col="blue", cex=0.5)
+				mapply(text, x=x[1]+exp(1/abs(out$yd)*.08)/4.25,y=out$yhlf+(out$y0-out$y1)/3, labels=paste(round(out$Pout*100,5),"%"), srt=out$srt2, cex=0.5,col=pcols[Pout])#col="blue"
 			}
 		}
 		points(x,y,pch=21,cex=5, bg=hcl.colors(4, "greens", 0.5, T)[litdifr[,,t]])
@@ -32,7 +34,7 @@ plotmovement <- function(){
 }
 #text(x=x[f]+.5,y=((y0+y1)*.5)[f], round(na.omit(orgins$Nout),6)[f],srt=90-atan((y1-y0)[f]*0.8)/pi*180-180,cex=1.2)
 	#arrows(x0=x*1.01,y0=orgins$y[1], y1=xy[xy$i==orgins[1,]$out2_i & xy$j==orgins[1,]$out2_j,]$y *.95, x1=x*1.09, length=0.1, lwd=orgins$Nout[1]*2*10^5)
-	
+#plot(1:75,pch=19,col=hcl.colors(75,"Blues 2"),cex=5)
 ij2y <- function(i,j,y){
 	xy <- expand.grid(i=1:nr,j=1:nc)
 	xy <- matrix(1:4,nc=nc)
@@ -63,3 +65,23 @@ trackOr <- function(dif){
 	out$srt2 <- yd/abs(yd)*exp(abs(yd)*.75)*35#(y1-y0)*150
 	return(out)
 }	
+
+colPout <- function(Pout,colorbar=T){
+	#n<-length(unique(round(Pout*100)))
+	#n <- n+3
+	n <- 5
+	pcols <- hcl.colors(n+6,"Blues 3",rev=T)[-1:-5][-(n+(1))]
+	#plot(1:n,pch=19,col=pcols,cex=5)
+	#plot(1:13,pch=19,col=hcl.colors(13,"Blues 2"),cex=5)
+	if (colorbar==T){
+		cbx <- timesteps-((n*5):1)*.025
+		segments(x0=cbx, y0=-.15,y1=-.1,col=rep(pcols,each=5),lwd=9,lend=3)
+		text(cbx[c(1,(1:5)*5)],-.08,labels=c(0,(1:5)*20))
+	}
+	return(pcols)
+}
+#colPout(Pout)
+#plotmovement()
+
+
+
